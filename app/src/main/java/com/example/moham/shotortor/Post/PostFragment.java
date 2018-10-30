85b164a10d059af503d673900c98d650bb61cfc8
mohammadhosseinbabaie <mohammadhosseinbabaie1996@yahoo.com>
2018-10-30 10:48:20 +0330

    toast test.

--- app/src/main/java/com/example/moham/shotortor/Post/PostFragment.java
+++ app/src/main/java/com/example/moham/shotortor/Post/PostFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.moham.shotortor.HomePage.R;
 
@@ -26,6 +27,7 @@ public class PostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_post, container, false);
+        Toast.makeText(getActivity(), "this is test", Toast.LENGTH_SHORT).show();
 
         return view;
     }
