16a1628a194d9c653df33a4bfd26b386ecd39214
mohammadhosseinbabaie <mohammadhosseinbabaie1996@yahoo.com>
2018-11-13 21:00:44 +0330

    test recycler views.

--- app/src/main/java/com/example/moham/shotortor/Post/PostFragment.java
+++ app/src/main/java/com/example/moham/shotortor/Post/PostFragment.java
@@ -2,24 +2,29 @@ package com.example.moham.shotortor.Post;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.moham.shotortor.HomePage.R;
+import com.example.moham.shotortor.Models.Photo;
 import com.example.moham.shotortor.Models.Rest;
 import com.example.moham.shotortor.WebServiece.APIClient;
 import com.example.moham.shotortor.WebServiece.APIInterface;
+import com.squareup.picasso.Picasso;
 
-import java.util.Objects;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
 
 public class PostFragment extends Fragment {
 
@@ -28,6 +33,9 @@ public class PostFragment extends Fragment {
     private static final String EXTRAS = "url_s";
     private static final String FORMAT = "json";
     private static final int NO_JSON_CALLBACK = 1;
+
+    private RecyclerView mRecyclerView;
+    private PhotoAdapter mPhotoAdapter;
 
     static PostFragment newInstance() {
 
@@ -42,6 +50,8 @@ public class PostFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_post, container, false);
+        mRecyclerView = view.findViewById(R.id.recycler);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),2));
         Toast.makeText(getActivity(), "this is test", Toast.LENGTH_SHORT).show();
 
         APIClient.getClient().create(APIInterface.class)
@@ -57,8 +67,8 @@ public class PostFragment extends Fragment {
             @Override
             public void onResponse(@NonNull Call<Rest> call, @NonNull Response<Rest> response) {
                 if (response.isSuccessful()) {
+                    updateUI(response.body().getPhotoes().getPhotoList());
                     Toast.makeText(getActivity(), "ok", Toast.LENGTH_SHORT).show();
-                    Toast.makeText(getActivity(), Objects.requireNonNull(response.body()).getPhotoes().getPhotoList().size() + "", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -70,4 +80,66 @@ public class PostFragment extends Fragment {
         return view;
     }
 
+    private void updateUI(List<Photo> photoList) {
+
+        if (mPhotoAdapter == null) {
+            mPhotoAdapter = new PhotoAdapter(photoList);
+            mRecyclerView.setAdapter(mPhotoAdapter);
+        } else {
+            mPhotoAdapter.setPhotoList(photoList);
+            mPhotoAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+
+        private List<Photo> photoList;
+
+        public PhotoAdapter(List<Photo> photoList) {
+
+            this.photoList = photoList;
+            Log.d("TAG" , photoList.size()+"");
+        }
+
+        public void setPhotoList(List<Photo> photoList) {
+            this.photoList = photoList;
+        }
+
+        @NonNull
+        @Override
+        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View view = inflater.inflate(R.layout.recycler_frame, parent, false);
+            Log.d("TAG" , "1");
+            return new PhotoHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PhotoHolder holder, int position) {
+            Photo photo = photoList.get(position);
+            Log.d("TAG" , "2");
+            holder.bindPhoto(photo);
+        }
+
+        @Override
+        public int getItemCount() {
+            return photoList.size();
+        }
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+
+        ImageButton mImageButton;
+
+        public PhotoHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mImageButton = itemView.findViewById(R.id.image_btn_recycler_frame);
+        }
+
+        public void bindPhoto(Photo photo) {
+            Log.d("TAG" , "ok");
+            Picasso.get().load(photo.getUrl()).into(mImageButton);
+        }
+    }
 }
